39d60c6c98522f2ed7da0ee0799fc0ee9b58d35f
HIMANSHU <126444642+himanshu8github@users.noreply.github.com>
2024-01-14 17:13:49 +0000

    variable discussion

---
+++ 1-BASICS/1-variable.js
@@ -0,0 +1,16 @@
+const accountId = 44889
+let accountEmail = "himanshu@google.com"
+var accountPassword = "12333"
+accountCity = "Delhi"
+let accountState;
+
+// accountId = 22--- not allowed becoz it is const value
+accountEmail = "himanshu@gmail.com"
+accountPassword = "119007"
+accountCity = "Manali"
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+// console.log(accountEmail)
+// console.log(accountPassword)
+// console.log(accountCity)
