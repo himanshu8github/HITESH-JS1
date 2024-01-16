06b3feef72d0d95d706076becbe565a8690343b0
HIMANSHU <126444642+himanshu8github@users.noreply.github.com>
2024-01-16 05:28:10 +0000

    datatypes discussion

---
+++ 1-BASICS/2-datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as newer version
+
+let name = "himanshu"
+let age = 12
+let isLoggedIn= true
+
+
+// number => 2 to the power 53
+// string
+// boolean
+// bigInt
+// null=> also null is defined as object in js....
+// undefined
+// symbol => unique
+
+//object
+
+console.log(typeof null)
+console.log(typeof undefined)
